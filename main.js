4c34c0958b50d34df3bb58ac6d94587e1fc18333
Rachel Colby <rachelcolby11@gmail.com>
2015-10-22 23:21:36 -0700

    Add photos and titles back into for loop

--- main.js
+++ main.js
@@ -11,20 +11,13 @@ request.onload = function() {
     data = JSON.parse(request.responseText);
     console.log("successfully parsed JSON");
     for (var i=0; i < data.photos.photo.length; i++) {
-      console.log(data.photos.photo[i].id);
+      item = data.photos.photo[i]
+      src = "https://farm"+ item.farm +".static.flickr.com/"+ item.server +"/"+ item.id +"_"+ item.secret +"_m.jpg";
+      $("<img/>").attr("src", src).appendTo("#images");
+      var title = document.createTextNode(item.title + "; ");
+      document.getElementById("images").appendChild(title);
     }
    } else {
     console.log("error");
    }
 };
-
-// $.getJSON(url, function(data) {})
-
-// .done(function(data) {
-//   $.each(data.photos.photo, function(i,item){
-//     src = "https://farm"+ item.farm +".static.flickr.com/"+ item.server +"/"+ item.id +"_"+ item.secret +"_m.jpg";
-//     $("<img/>").attr("src", src).appendTo("#images");
-//     var title = document.createTextNode(item.title + "; ");
-//     document.getElementById("images").appendChild(title);
-//   });
-// });
